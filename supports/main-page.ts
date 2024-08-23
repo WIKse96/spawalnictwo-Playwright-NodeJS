099add07ae9fa3f0597232c740b1780d4ae6b10e
WIKse96 <105929893+WIKse96@users.noreply.github.com>
2024-08-23 15:45:21 +0200

    Update main-page.ts

--- supports/main-page.ts
+++ supports/main-page.ts
@@ -9,38 +9,37 @@ export class MainPage {
   readonly accept_cookie_btn: Locator;
   readonly reject_cookie_btn: Locator;
 
-
-constructor(page: Page){
+  constructor(page: Page) {
     this.page = page;
     this.h1 = page.getByRole('heading');
     this.search = page.locator("//input[@placeholder='Wpisz czego szukasz']");
     this.cookie_container = page.getByText('This page uses cookie files');
-    this.accept_cookie_btn = page.getByText('I DO NOT AGREE')
-    this.reject_cookie_btn = page.getByText('I AGREE, I WANT TO GO TO THE')
+    this.accept_cookie_btn = page.getByText('I AGREE, I WANT TO GO TO THE');
+    this.reject_cookie_btn = page.getByText('I DO NOT AGREE');
     this.context = this.page.context();
+  }
 
-}
-async goto(url: string){
-   await this.page.goto(url);
-}
+  async goto(url: string) {
+    await this.page.goto(url);
+  }
 
-async clear_cookies(){
-    await this.context.clearCookies();    
-}
+  async clear_cookies() {
+    await this.context.clearCookies();
+  }
 
-async verify_cookie_container(){
+  async verify_cookie_container() {
     await expect(this.cookie_container).toBeVisible();
-}
-async accept_cookies(){
+  }
+
+  async accept_cookies() {
     await this.accept_cookie_btn.click();
-}
+  }
 
-async reject_cookies(){
+  async reject_cookies() {
     await this.reject_cookie_btn.click();
-}
+  }
 
-async verivy_cookie_container_invisible(){
-    expect(this.cookie_container).toBeHidden();
-}
-
+  async verivy_cookie_container_invisible() {
+    await expect(this.cookie_container).toBeHidden();
+  }
 }
