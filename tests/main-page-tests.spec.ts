ef54cf6a098310fb3d03a9ac8f97c65417324106
WIKse96 <105929893+WIKse96@users.noreply.github.com>
2024-08-23 15:45:40 +0200

    Update main-page-tests.spec.ts

--- tests/main-page-tests.spec.ts
+++ tests/main-page-tests.spec.ts
@@ -1,58 +1,53 @@
 import { test, expect } from '@playwright/test';
 import { MainPage } from '../supports/main-page';
 import { Storages } from '../supports/storages_hanlder';
-import {Menu} from '../supports/components/menu';
+import { Menu } from '../supports/components/menu';
 
+test.describe('Main Page Tests', () => {
+  let mainPage: MainPage;
+  let storageHandler: Storages;
+  let menu: Menu;
 
+  test.beforeEach(async ({ page }) => {
+    mainPage = new MainPage(page);
+    storageHandler = new Storages(page);
+    menu = new Menu(page);
+  });
 
-test('Main page entry tests', async ({ page }) => {
-  const main_page = new MainPage(page);
-  await main_page.goto('/');
-  // Sprawdź czy strona ma tytuł.
-  await expect(page).toHaveTitle(/Main page/);
+  test('Main page entry test', async ({ page }) => {
+    await mainPage.goto('/');
+    await expect(page).toHaveTitle(/Main page/);
+  });
+
+  test('Reject cookies', async ({ page }) => {
+    await mainPage.goto('/');
+    await mainPage.verify_cookie_container();
+    await mainPage.reject_cookies();
+    await mainPage.verivy_cookie_container_invisible();
+    await storageHandler.save_storage('rejected_cookie');
+    await mainPage.clear_cookies();
+  });
+
+  test('Accept cookies', async ({ page }) => {
+    await mainPage.goto('/');
+    await mainPage.verify_cookie_container();
+    await mainPage.accept_cookies();
+    await storageHandler.save_storage('accepted_cookie');
+    await mainPage.verivy_cookie_container_invisible();
+    await mainPage.clear_cookies();
+  });
+
+  test('Check main page with accepted cookies', async ({ page }) => {
+    await mainPage.goto('/');
+    await storageHandler.read_storage('accepted');
+    await page.reload();
+    await expect(page.getByRole('link', { name: 'Producenci' })).toBeVisible();
+  });
+
+  test('Menu tests', async ({ page }) => {
+    await mainPage.goto('/');
+    await storageHandler.read_storage('accepted');
+    await page.reload();
+    // TODO: Implement tests for menu interactions
+  });
 });
-
-test('Reject cookies', async ({ page }) => {
-  const main_page = new MainPage(page);
-  const storage_handler = new Storages(page)
-  await main_page.goto('/');
-  await main_page.verify_cookie_container();
-  await main_page.reject_cookies();
-  await main_page.verivy_cookie_container_invisible();
-  // Parametr save_storage (nazwa pliku)
-  await storage_handler.save_storage('rejected_cookie')
-  await main_page.clear_cookies();
-
-
-});
-test('Accept cookies', async ({ page }) => {
-  const main_page = new MainPage(page);
-  const storage_handler = new Storages(page)
-  await main_page.goto('/');
-  await main_page.verify_cookie_container();
-
-  await main_page.accept_cookies();
-  await storage_handler.save_storage('accepted_cookie')
-  await main_page.verivy_cookie_container_invisible();
-  await main_page.clear_cookies()
-
-});
-
-test('Check main page', async({page})=>{
-  const main_page = new MainPage(page);
-  const storage_handler = new Storages(page)
-  await main_page.goto('/');
-  await storage_handler.read_storage('accepted');
-  await page.reload();
-  expect(page.getByRole('link', { name: 'Producenci' })).toBeVisible;
-
-})
-test('Menu tests', async ({page})=>{
-  const main_page = new MainPage(page);
-  const storage_handler = new Storages(page);
-  const menu = new Menu(page)
-  await main_page.goto('/');
-  await storage_handler.read_storage('accepted');
-  await page.reload();
-  // TODO: Stworzyc testy dla menu
-})
